d40976acdd7be02db3f5694866e4c2df4251d4b5
b2nkuu <ban.kuu@yahoo.com>
2024-05-25 09:25:56 +0700

    Initial commit

---
+++ lua/chadrc.lua
@@ -0,0 +1,30 @@
+-- This file  needs to have same structure as nvconfig.lua
+-- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
+
+---@type ChadrcConfig
+local M = {}
+
+M.ui = {
+  theme = "oxocarbon",
+  transparency = true,
+  nvdash = {
+    load_on_startup = true,
+    header = {
+      "██████╗ ██████╗ ███╗   ██╗██╗  ██╗██╗   ██╗██╗   ██╗",
+      "██╔══██╗╚════██╗████╗  ██║██║ ██╔╝██║   ██║██║   ██║",
+      "██████╔╝ █████╔╝██╔██╗ ██║█████╔╝ ██║   ██║██║   ██║",
+      "██╔══██╗██╔═══╝ ██║╚██╗██║██╔═██╗ ██║   ██║██║   ██║",
+      "██████╔╝███████╗██║ ╚████║██║  ██╗╚██████╔╝╚██████╔╝",
+      "╚═════╝ ╚══════╝╚═╝  ╚═══╝╚═╝  ╚═╝ ╚═════╝  ╚═════╝ ",
+    },
+  },
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
+  statusline = {
+    separator_style = "arrow",
+  },
+}
+
+return M
